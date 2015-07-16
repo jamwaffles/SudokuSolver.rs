1c77781cae67db00a9b1dd020471ad39faaad333
James Waples <jamwaffles@gmail.com>
2015-07-16 18:36:00 +0100

    Print original board before solving

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
 mod sudoku;
 
 fn main() {
-	// let input_board = vec![
-	// 	vec![ 7,0,0,0,2,0,0,0,4 ],
-	// 	vec![ 8,0,3,0,4,0,9,0,7 ],
-	// 	vec![ 0,0,2,0,0,0,3,0,0 ],
-	// 	vec![ 0,7,0,0,0,0,0,0,0 ],
-	// 	vec![ 0,6,0,9,0,5,0,1,0 ],
-	// 	vec![ 0,0,0,0,0,0,0,8,0 ],
-	// 	vec![ 0,0,9,0,0,0,6,0,0 ],
-	// 	vec![ 4,0,6,0,5,0,8,0,1 ],
-	// 	vec![ 5,0,0,0,3,0,0,0,2 ],
-	// ];
+	let mut puzzle = sudoku::Board::import("./boards/board1.txt");
 
-	let input_board = vec![
-		vec![ 0,8,3,0,0,7,0,2,0 ],
-		vec![ 0,0,9,0,3,0,6,7,4 ],
-		vec![ 0,0,0,0,2,0,0,0,0 ],
-		vec![ 0,0,0,0,0,4,0,0,9 ],
-		vec![ 0,0,0,7,0,0,2,0,0 ],
-		vec![ 8,6,0,2,0,0,3,1,0 ],
-		vec![ 0,0,7,0,0,9,0,6,0 ],
-		vec![ 0,0,0,0,0,0,0,0,3 ],
-		vec![ 0,1,0,0,0,0,8,9,0 ],
-	];
+	println!("Original board:");
+	println!("");
 
-	let mut puzzle = sudoku::Board::new(&input_board);
+	puzzle.print_board();
 
 	puzzle.solve();
 
